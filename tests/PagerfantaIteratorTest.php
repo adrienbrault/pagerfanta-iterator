2a932d9cc79236a0961ef8fd2fbed0a5040b315e
Thomas Tourlourat <thomas@tourlourat.com>
2018-05-30 17:06:35 +0200

    test factories

--- tests/PagerfantaIteratorTest.php
+++ tests/PagerfantaIteratorTest.php
@@ -45,4 +45,42 @@ class PagerfantaIteratorTest extends ProphecyTestCase
             iterator_to_array($pagerIterator)
         );
     }
+    
+    public function testIterateOverPagesFactory()
+    {
+        $adapterProphecy = $this->prophesize('Pagerfanta\Adapter\AdapterInterface');
+        $adapterProphecy->getNbResults()->willReturn(18);
+        $adapterProphecy->getSlice(0, 5)->willReturn(range(0, 4))->shouldBeCalledTimes(1);
+        $adapterProphecy->getSlice(5, 5)->willReturn(range(5, 9))->shouldBeCalledTimes(1);
+        $adapterProphecy->getSlice(10, 5)->willReturn(range(10, 14))->shouldBeCalledTimes(1);
+        $adapterProphecy->getSlice(15, 5)->willReturn(range(15, 18))->shouldBeCalledTimes(1);
+
+        $pager = new Pagerfanta($adapterProphecy->reveal());
+        $pager->setMaxPerPage(5);
+
+        $pagerIterator = new IteratorIterator(PagerfantaIterator::iterateOverPages($pager));
+        $this->assertSame(
+            range(0, 18),
+            iterator_to_array($pagerIterator)
+        );
+    }
+
+    public function testIterateOverElementsFactory()
+    {
+        $adapterProphecy = $this->prophesize('Pagerfanta\Adapter\AdapterInterface');
+        $adapterProphecy->getNbResults()->willReturn(18);
+        $adapterProphecy->getSlice(0, 5)->willReturn(range(0, 4))->shouldBeCalledTimes(1);
+        $adapterProphecy->getSlice(5, 5)->willReturn(range(5, 9))->shouldBeCalledTimes(1);
+        $adapterProphecy->getSlice(10, 5)->willReturn(range(10, 14))->shouldBeCalledTimes(1);
+        $adapterProphecy->getSlice(15, 5)->willReturn(range(15, 18))->shouldBeCalledTimes(1);
+
+        $pager = new Pagerfanta($adapterProphecy->reveal());
+        $pager->setMaxPerPage(5);
+
+        $pagerIterator = PagerfantaIterator::iterateOverElements($pager);
+        $this->assertSame(
+            range(0, 18),
+            iterator_to_array($pagerIterator)
+        );
+    }
 }
